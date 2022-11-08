72a62b3780c701eb0fcf521a8e7233789f1fa975
Douglas Meürer <douggmeurer@gmail.com>
2022-11-08 11:27:17 -0300

    button componenet

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,26 +1,75 @@
 import { useState } from "react";
+import Button from './components/Button'
 
 function App() {
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+    })
 
-function addGood() {
-  setGood(currGood => currGood + 1)
+  const [allClicks, setAllClicks] = useState([])
+
+function handleGood () {
+  setClicks({...clicks, good: clicks.good + 1})
+  setAllClicks(allClicks.concat(1))
 }
+
+function handleNeutral () {
+  setClicks({...clicks, neutral: clicks.neutral + 1})
+  setAllClicks(allClicks.concat(0))
+}
+
+function handleBad () {
+  setClicks({...clicks, bad: clicks.bad + 1})
+  setAllClicks(allClicks.concat(-1))
+}
+
+let newGood = []
+let newNeutral = []
+let newBad = []
+
+  allClicks.forEach(number => {
+ 
+    if (number === 1) {
+      return newGood.push(number)
+      
+    } 
+
+    if (number === 0) {
+      return newNeutral.push(number)
+      
+    } 
+
+    if (number === -1) {  
+      return newBad.push(number)
+
+    }
+  })
+
+  console.log("newgood", typeof newGood)
+  console.log("newneutral",newNeutral.length)
+  console.log("newbad",newBad.length)
+  console.log("avg", newGood.length + newBad.lenght)
+
+
+console.log(clicks)
+console.log(allClicks)
 
   return (
     <div className="App">
     <h1>Give Feedback!</h1>
-    <button  onClick={addGood}>Good</button>
-    <button  onClick={() => {setNeutral(currNeu => currNeu + 1)}}>Neutral</button>
-    <button  onClick={() => {setBad(currBad => currBad + 1)}}>Bad</button>
+    <Button onClick={handleGood} text="Good"></Button>
+    <Button onClick={handleNeutral} text="Neutral"></Button>
+    <Button onClick={handleBad} text="Bad"></Button>
     <div>
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <p>Good: {clicks.good}</p>
+      <p>Neutral: {clicks.neutral}</p>
+      <p>Bad: {clicks.bad}</p>
+      <p>All: {allClicks.length}</p>
+      <p>Average: {}</p>
     </div>
     </div>
   );
